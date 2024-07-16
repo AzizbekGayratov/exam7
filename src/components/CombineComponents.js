25f0349414394f5820ffb94c5d3474961a5629ee
AzizbekGayratov <azizbekgairatov@gmail.com>
2024-07-17 03:23:11 +0500

    init

---
+++ src/components/CombineComponents.js
@@ -0,0 +1,6 @@
+import Cart from "./cart/Cart";
+import Home from "./home/Home";
+import Products from "./products/Products";
+import SingleProduct from "./SingleProduct";
+
+export { Cart, Home, Products, SingleProduct };
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="main__content">
+      <div>Home</div>
+    </div>
+  );
+};
+
+export default Home;
